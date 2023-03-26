9cbdc2fe6d7f803a6cd67e6328ff638f2de233fb
portalplusbot <128649752+benjisqt@users.noreply.github.com>
2023-03-26 18:20:31 +0100

    Added MC info command and nuke channel command

---
+++ commands/info/minecraft.js
@@ -0,0 +1,189 @@
+const {
+    SlashCommandBuilder,
+    ChatInputCommandInteraction,
+    EmbedBuilder,
+    AttachmentBuilder
+} = require('discord.js');
+const fetch = require('node-fetch');
+const { Reply } = require('../../util/replies');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('minecraft')
+        .setDescription('Minecraft-related commands')
+        .addSubcommand((sub) =>
+            sub.setName('profile')
+            .setDescription('Information about a Minecraft player')
+            .addStringOption((opt) =>
+                opt.setName('username')
+                .setDescription('The username of the Minecraft account')
+                .setRequired(true)
+            )
+            .addBooleanOption((opt) =>
+                opt.setName('silent')
+                .setDescription('Whether command feedback should only be sent to you')
+            )
+        )
+        .addSubcommand((sub) =>
+            sub.setName('server')
+            .setDescription('Information about a Java edition or Bedrock edition Minecraft server')
+            .addStringOption((opt) =>
+                opt.setName('address')
+                .setDescription('The address of the Minecraft server')
+                .setRequired(true)
+            )
+            .addStringOption((opt) =>
+                opt.setName('type')
+                .setDescription('The type of server')
+                .addChoices({
+                    name: 'Bedrock Server',
+                    value: 'bedrock'
+                }, {
+                    name: 'Java Edition Server',
+                    value: 'java'
+                }, )
+                .setRequired(true)
+            )
+            .addBooleanOption((opt) =>
+                opt.setName('silent')
+                .setDescription('Whether command feedback should only be sent to you')
+            )
+        ),
+
+    /**
+     * @param {ChatInputCommandInteraction} interaction
+     */
+
+    async execute(interaction) {
+        const sub = interaction.options.getSubcommand();
+        const silent = interaction.options.getBoolean('silent') || false;
+
+        switch(sub) {
+            case 'profile' : {
+                const user = interaction.options.getString('username');
+
+                const data = await fetch(`https://api.minetools.eu/uuid/${user}`).then(res => res.json());
+
+                if(data.status === 'ERR') return Reply(interaction, 'Red', '🚫', `That username was not found. Please make sure you typed it correctly.`, true);
+
+                const dataa = await fetch(`https://crafatar.com/avatars/${data.id}`).then(res => res.buffer());
+
+                if(silent === true) {
+                    return interaction.reply({
+                        embeds: [
+                            new EmbedBuilder()
+                            .setTitle(`${user}`)
+                            .addFields(
+                                {
+                                    name: 'Skin',
+                                    value: `[Download](https://crafatar.com/skins/${data.id})`,
+                                },
+                                {
+                                    name: 'Cape',
+                                    value: `[Download](https://crafatar.com/capes/${data.id})`,
+                                },
+                                {
+                                    name: '3D Head Model',
+                                    value: `[Download](https://crafatar.com/renders/head/${data.id})`
+                                },
+                                {
+                                    name: 'UUID',
+                                    value: `${data.id}`
+                                }
+                            )
+                            .setColor('Random')
+                            .setThumbnail(`https://crafatar.com/renders/body/${data.id}`)
+                        ], ephemeral: true
+                    });
+                } else {
+                    return interaction.reply({
+                        embeds: [
+                            new EmbedBuilder()
+                            .setTitle(`${user}`)
+                            .addFields(
+                                {
+                                    name: 'Skin',
+                                    value: `[Download](https://crafatar.com/skins/${data.id})`,
+                                },
+                                {
+                                    name: 'Cape',
+                                    value: `[Download](https://crafatar.com/capes/${data.id})`,
+                                },
+                                {
+                                    name: '3D Head Model',
+                                    value: `[Download](https://crafatar.com/renders/head/${data.id})`
+                                },
+                                {
+                                    name: 'UUID',
+                                    value: `${data.id}`
+                                }
+                            )
+                            .setColor('Random')
+                            .setThumbnail(`https://crafatar.com/renders/body/${data.id}`)
+                        ]
+                    });
+                }
+            }
+            break;
+
+            case 'server' : {
+                let site;
+
+                const address = interaction.options.getString('address');
+                const type = interaction.options.getString('type');
+
+                if(type === 'bedrock') site = `https://api.mcsrvstat.us/bedrock/2/`;
+                if(type === 'java') site = `https://api.mcsrvstat.us/2/`;
+
+                const data = await fetch(`${site}${address}`).then(res => res.json());
+
+                if(data.ping === false) return Reply(interaction, 'Red', '🚫', `That server was not found. Please ensure you selected the right address and version.`, true);
+
+                if(type === 'bedrock') {
+                    if(silent === true) {
+                        return interaction.reply({
+                            embeds: [
+                                new EmbedBuilder()
+                                .setTitle(`Minecraft Server Information: ${address} - ${type}`)
+                                .setDescription(`\`\`\`Server Address: ${data.hostname}:${data.port}\nMotd: ${data.motd.clean}\nPlayers Online: ${data.players.online}\nMax Players: ${data.players.max}\nVersion: ${data.version}\nOnline: ${data.online}\nSoftware: ${data.software}\nGamemode: ${data.gamemode}\`\`\``)
+                                .setColor('Random')
+                            ], ephemeral: true
+                        })
+                    } else {
+                        return interaction.reply({
+                            embeds: [
+                                new EmbedBuilder()
+                                .setTitle(`Minecraft Server Information: ${address} - ${type}`)
+                                .setDescription(`\`\`\`Server Address: ${data.hostname}:${data.port}\nMotd: ${data.motd.clean}\nPlayers Online: ${data.players.online}\nMax Players: ${data.players.max}\nVersion: ${data.version}\nOnline: ${data.online}\nSoftware: ${data.software}\nGamemode: ${data.gamemode}\`\`\``)
+                                .setColor('Random')
+                            ]
+                        })
+                    }
+                }
+
+                if(type === 'java') {
+                    if(silent === true) {
+                        return interaction.reply({
+                            embeds: [
+                                new EmbedBuilder()
+                                .setTitle(`Minecraft Server Information: ${address} - ${type}`)
+                                .setDescription(`\`\`\`Server Address: ${data.hostname}:${data.port}\nMotd: ${data.motd.clean}\nPlayers Online: ${data.players.online}\nMax Players: ${data.players.max}\nVersion: ${data.version}\nOnline: ${data.online}\nSoftware: ${data.software}\`\`\``)
+                                .setColor('Random')
+                            ], ephemeral: true
+                        })
+                    } else {
+                        return interaction.reply({
+                            embeds: [
+                                new EmbedBuilder()
+                                .setTitle(`Minecraft Server Information: ${address} - ${type}`)
+                                .setDescription(`\`\`\`Server Address: ${data.hostname}:${data.port}\nMotd: ${data.motd.clean}\nPlayers Online: ${data.players.online}\nMax Players: ${data.players.max}\nVersion: ${data.version}\nOnline: ${data.online}\nSoftware: ${data.software}\`\`\``)
+                                .setColor('Random')
+                            ]
+                        })
+                    }
+                }
+            }
+            break;
+        }
+    }
+}
--- commands/moderation/textch.js
+++ commands/moderation/textch.js
@@ -36,7 +36,15 @@ module.exports = {
                 opt.setName('silent')
                 .setDescription('Whether command feedback should only be sent to you')
             )
-        ),
+        )
+        .addSubcommand((sub) =>
+        sub.setName('nuke')
+        .setDescription('Clones the channel and deletes the old one')
+        .addBooleanOption((opt) =>
+            opt.setName('silent')
+            .setDescription('Whether command feedback should only be sent to you')
+        )
+    ),
 
     /**
      * @param {ChatInputCommandInteraction} interaction
@@ -47,26 +55,25 @@ module.exports = {
         const silent = interaction.options.getBoolean('silent') || false;
         const role = interaction.options.getRole('role');
 
-        const validrole = interaction.guild.roles.cache.find(r => r.id === role.id);
-        if(!validrole) return Reply(interaction, 'Red', '🚫', 'This role was not found in this guild.', true)
-        if(validrole.position >= interaction.guild.members.me.roles.highest.position) return Reply(
-            interaction,
-            'Red',
-            '🚫',
-            `The select role has a higher position than my highest role.\nPlease make my role higher than the selected role.`,
-            true
-        );
-
-        if(validrole.position >= interaction.member.roles.highest.position) return Reply(
-            interaction,
-            'Red',
-            '🚫',
-            `The select role has a higher position than your highest role.\nPlease make your role higher than the selected role.`,
-            true
-        );
-
         switch (sub) {
             case 'lock': {
+                const validrole = interaction.guild.roles.cache.find(r => r.id === role.id);
+                if(!validrole) return Reply(interaction, 'Red', '🚫', 'This role was not found in this guild.', true)
+                if(validrole.position >= interaction.guild.members.me.roles.highest.position) return Reply(
+                    interaction,
+                    'Red',
+                    '🚫',
+                    `The select role has a higher position than my highest role.\nPlease make my role higher than the selected role.`,
+                    true
+                );
+        
+                if(validrole.position >= interaction.member.roles.highest.position) return Reply(
+                    interaction,
+                    'Red',
+                    '🚫',
+                    `The select role has a higher position than your highest role.\nPlease make your role higher than the selected role.`,
+                    true
+                );
                 interaction.channel.permissionOverwrites.create(role.id, { SendMessages: false });
                 if(silent === true) {
                     return Reply(interaction, 'Green', '✅', 'Members will no longer be able to send messages in this channel unless the channel is unlocked.', true);
@@ -77,12 +84,43 @@ module.exports = {
             break;
 
             case 'unlock': {
+                const validrole = interaction.guild.roles.cache.find(r => r.id === role.id);
+                if(!validrole) return Reply(interaction, 'Red', '🚫', 'This role was not found in this guild.', true)
+                if(validrole.position >= interaction.guild.members.me.roles.highest.position) return Reply(
+                    interaction,
+                    'Red',
+                    '🚫',
+                    `The select role has a higher position than my highest role.\nPlease make my role higher than the selected role.`,
+                    true
+                );
+        
+                if(validrole.position >= interaction.member.roles.highest.position) return Reply(
+                    interaction,
+                    'Red',
+                    '🚫',
+                    `The select role has a higher position than your highest role.\nPlease make your role higher than the selected role.`,
+                    true
+                );
                 interaction.channel.permissionOverwrites.create(role.id, { SendMessages: true });
                 if(silent === true) {
                     return Reply(interaction, 'Green', '✅', 'Members are now able to send messages in this channel.', true);
                 } else {
                     return Reply(interaction, 'Green', '✅', 'Members are now able to send messages in this channel.', false);
                 }
+            }
+            break;
+
+            case 'nuke' : {
+                const ch = interaction.channel;
+                ch.clone().then(async newch => {
+                    if(silent === false) {
+                        const msg = await newch.send({ content: `Channel has been cleared of messages!` });
+                        setTimeout(() => {
+                            msg.delete();
+                        }, 3000);
+                    }
+                    ch.delete();
+                })
             }
             break;
         }
