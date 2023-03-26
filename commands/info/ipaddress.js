330a00e7a9cd79e4aa15b419372c61063185dcf2
portalplusbot <128649752+benjisqt@users.noreply.github.com>
2023-03-26 03:48:23 +0100

    Fixed and improved ip command

--- commands/info/ipaddress.js
+++ commands/info/ipaddress.js
@@ -4,7 +4,7 @@ const { Reply } = require('../../util/replies');
 
 module.exports = {
     data: new SlashCommandBuilder()
-    .setName('ip')
+    .setName('ipaddress')
     .setDescription('Provides detailed information about an IP address')
     .addStringOption((opt) =>
         opt.setName('ip')
@@ -17,17 +17,19 @@ module.exports = {
      */
 
     async execute(interaction) {
+        await interaction.deferReply();
         const ip = interaction.options.getString('ip');
 
         await fetch(`http://ip-api.com/json/${ip}`).then(res => res.json()).then(res => {
-            if(res.success === 'fail') return interaction.channel.send(interaction, 'Red', '🚫', `There was an error finding information for that IP address.\nTry making sure you entered it correctly.`, true);
+            if(!res.country) return interaction.editReply(interaction, 'Red', '🚫', `There was an error finding information for that IP address.\nTry making sure you entered it correctly.`, true);
 
-            if(res.success === 'success') {
-                return interaction.channel.send({
+            if(res.country) {
+                return interaction.editReply({
                     embeds: [
                         new EmbedBuilder()
                         .setTitle(`IP information for ${ip}`)
                         .setDescription(`\`\`\`Country: ${res.country}\nCountry Code: ${res.countryCode}\nRegion: ${res.regionName}\nCity: ${res.city}\nZip/Post Code: ${res.zip}\nTime Zone: ${res.timezone}\nISP: ${res.isp}\nOrganisation: ${res.org}\`\`\``)
+                        .setColor('Random')
                     ]
                 })
             }
