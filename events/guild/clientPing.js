dd9082541dd0f69e0bd20b2cf6ac2ce11bc8781b
benji <128649752+benjisqt@users.noreply.github.com>
2023-05-20 01:56:23 +0100

    Visual refinements

--- events/guild/clientPing.js
+++ events/guild/clientPing.js
@@ -14,16 +14,20 @@ module.exports = {
         if(!message.inGuild()) return;
         if(message.mentions.members.has(client.user.id)) {
             if(message.channel.permissionsFor(client.user).has('SendMessages')) {
-                return message.reply({
+                const msg = await message.reply({
                     embeds: [
                         new EmbedBuilder()
                         .setTitle(`Hey! I'm Portal+`)
                         .setDescription(`Friendly reminder, my prefix is /, I only support slash commands! :)\nHave a good day!`)
-                        .setFooter({ text: `Portal+ being friendly?!` })
+                        .setFooter({ text: `portal+ being friendly?!` })
                         .setColor('Gold')
                         .setThumbnail(client.user.displayAvatarURL({ size: 1024 }))
                     ]
-                })
+                });
+
+                setTimeout(() => {
+                    msg.delete();
+                }, 3000);
             } else return;
         } else return;
     }
