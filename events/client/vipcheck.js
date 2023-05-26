328b36ae2bb9bac99943c88953c0743da101e0a7
benjis <benjiallan07@icloud.com>
2023-05-27 00:30:20 +0100

    Fixed VIP Checking Error!

--- events/client/vipcheck.js
+++ events/client/vipcheck.js
@@ -14,30 +14,6 @@ module.exports = {
         const data = await vip.find();
         if(!data) return;
 
-        data.forEach(date => {
-            if(Date.now() >= date.Expires) {
-                const guilds = client.guilds.cache.get(date.Guild);
-                const validch = guilds.channels.cache.filter(ch => ch.permissionsFor(guilds.members.me).has('SendMessages') && ch.type === ChannelType.GuildText).first();
-                if(!validch) return;
-                validch.send({ embeds: [
-                    new EmbedBuilder()
-                    .setTitle(`VIP License Expired`)
-                    .setDescription(`Your VIP license has expired. This means you will not be able to use VIP commands until a new license is redeemed.`)
-                    .setColor('Gold')
-                    .addFields(
-                        {
-                            name: 'Duration',
-                            value: `${date.Duration}`
-                        }
-                    )
-                    .setThumbnail(guilds.iconURL({ dynamic: true }))
-                ] }).catch(err => {
-                    return;
-                })
-                date.deleteOne({ Guild: date.Guild });
-            }
-        })
-
         setInterval(async () => {
             const data = await vip.find();
             if(!data) return;
@@ -60,7 +36,7 @@ module.exports = {
                         )
                         .setThumbnail(guilds.iconURL({ dynamic: true }))
                     ] });
-                    date.deleteOne({ Guild: date.Guild });
+                    vip.deleteMany({ Guild: date.Guild });
                 }
             })
         }, 90000)
